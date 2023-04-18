bef45a549608b7fba4d86f68cffee4b6d9078bcf
Bence Farkas <bence.farkas01@gmail.com>
2023-04-18 10:06:53 +0200

    Added Message model

---
+++ la06.Backend/Message.cs
@@ -0,0 +1,18 @@
+﻿namespace la06.Backend
+{
+    public class Message
+    {
+
+        public string Content { get; set; }
+        public DateTime Sent { get; set; }
+        public string Sender { get; set; }
+
+        public Message(string content, DateTime sent, string sender)
+        {
+            Content = content;
+            Sent = sent;
+            Sender = sender;
+        }
+
+    }
+}
